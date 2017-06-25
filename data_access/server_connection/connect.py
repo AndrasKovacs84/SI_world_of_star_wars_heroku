4b4ad18ddb9302925442d5cd3c8d4668164c6e8b
András Kovács <ka841001@gmail.com>
2017-06-25 10:14:51 +0200

    psql access details

--- data_access/server_connection/connect.py
+++ data_access/server_connection/connect.py
@@ -1,20 +1,24 @@
 import psycopg2
 import sys
-from .personal_config import my_connection
+import os
+import urllib
 
 
 def connect_to_sql(func):
     def with_connection(*args):
+        urllib.parse.uses_netloc.append('postgres')
+        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
         connection = None
         sql_query_func = None
         try:
-            # setup connection string
-            connect_str = my_connection()
             # use our connection values to establish a connection
-            connection = psycopg2.connect(host=connect_str["host"],
-                                          user=connect_str["user"],
-                                          password=connect_str["passwd"],
-                                          dbname=connect_str["dbname"])
+            connection = psycopg2.connect(
+                database=url.path[1:],
+                user=url.username,
+                password=url.password,
+                host=url.hostname,
+                port=url.port
+            )
             # set autocommit option, to do every query when we call it
             connection.autocommit = True
             # create a psycopg2 cursor that can execute queries
